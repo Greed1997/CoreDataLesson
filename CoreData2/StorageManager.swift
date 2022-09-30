3ed8d2ec3ec9fa9c4b0bcfaff7d5a868b06b6953
Greed1997 <43263569+Greed1997@users.noreply.github.com>
2022-09-30 15:04:21 +0300

    alert

---
+++ CoreData2/StorageManager.swift
@@ -0,0 +1,16 @@
+//
+//  StorageManager.swift
+//  CoreData2
+//
+//  Created by Александр on 30.09.2022.
+//
+
+import Foundation
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    
+    
+    private init() {}
+}
